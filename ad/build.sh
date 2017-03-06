150c84819bebf0487283e20d41f147709da86195
patrickbucher <patrick.bucher@stud.hslu.ch>
2017-03-06 10:16:41 +0100

    added TODOs and rebuilt documents

--- ad/build.sh
+++ ad/build.sh
@@ -1,20 +1,21 @@
 rm -f ad.html
 rm -f ad.pdf
 
-title="Algorithmen \& Datenstrukturen"
-author="Patrick Bucher"
+title='Algorithmen \& Datenstrukturen'
+author='Patrick Bucher'
 
 pandoc -N -S -s -c style.css --toc \
-    --variable title="$title"\
+    --variable title="Algorithmen & Datenstrukturen" \
     --variable author="$author" \
     --variable lang=de\
     *.md -o ad.html
 
 pandoc -N -S -s --toc \
-    --variable title="$title"\
+    --variable title="$title" \
     --variable author="$author" \
     --variable papersize=a4 \
     --variable documentclass=scrartcl \
+    --variable classoption=smallheadings,11pt \
     --variable fontfamily=times \
     --variable lang=de\
     *.md -o ad.pdf
